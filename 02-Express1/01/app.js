07ff29c6dffb34b1e01f8e69a3a39f44caab34e4
siavash sattari <siavash.sattari.dev@gmail.com>
2022-07-13 07:43:04 -0700

    01 - routing

---
+++ 02-Express1/01/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({
+    status: true,
+    message: 'Showing the index page'
+  });
+});
+
+app.get('/auth/login', (req, res) => {
+  res.send({
+    status: true,
+    message: 'Showing the login page'
+  });
+});
+
+app.get('/profile/:userId', (req, res) => {
+  res.send({
+    status: true,
+    message: 'Showing the login page',
+    user_id: req.params.userId
+  });
+});
+
+app.get('/profile/:userId/addresses/:addressId', (req, res) => {
+  res.send({
+    status: true,
+    message: 'Showing the login page',
+    user_id: req.params.userId,
+    address_id: req.params.addressId
+  });
+});
+
+const startApplication = () => {
+  app.listen(3000, () => {
+    console.log('Application is running on port 3000');
+  });
+};
+
+module.exports = startApplication;
